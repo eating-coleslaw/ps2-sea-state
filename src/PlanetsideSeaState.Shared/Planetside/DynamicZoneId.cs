cec4fcfacde5469ec35ba98a7a7c08aaa62b575e
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2021-10-01 11:17:08 -0500

    update DynamicZoneId to use unsigned types, add tests

--- src/PlanetsideSeaState.Shared/Planetside/DynamicZoneId.cs
+++ src/PlanetsideSeaState.Shared/Planetside/DynamicZoneId.cs
@@ -5,30 +5,30 @@
         /// <summary>
         /// The ID of the dynamic zone
         /// </summary>
-        public int DefinitionId { get; } // zone ID
+        public ushort DefinitionId { get; } // zone ID
 
         /// <summary>
         /// The instance ID of the dynamic zone
         /// </summary>
-        public int InstanceId { get; }
+        public ushort InstanceId { get; }
 
         /// <summary>
         /// The zone ID value as it would appear in census stream payloads' zone_id field
         /// </summary>
-        public int ZoneId { get; }
+        public uint ZoneId { get; }
 
-        private const int _topMask = unchecked((int)0xFFFF0000);
-        private const int _bottomMask = 0x0000FFFF;
+        private const uint _topMask = 0xFFFF0000;
+        private const uint _bottomMask = 0x0000FFFF;
 
         /// <summary>
         /// Create a new DynamicZoneId from a census stream payload's zone_id field
         /// </summary>
         /// <param name="zoneId">The zone_id value from a census strean payload</param>
-        public DynamicZoneId(int zoneId)
+        public DynamicZoneId(uint zoneId)
         {
             ZoneId = zoneId;
-            DefinitionId = ApplyMask(zoneId, _bottomMask);
-            InstanceId = ApplyBitShiftRight(ApplyMask(zoneId, _topMask));
+            DefinitionId = (ushort)ApplyMask(zoneId, _bottomMask);
+            InstanceId = (ushort)ApplyBitShiftRight(ApplyMask(zoneId, _topMask));
         }
 
         /// <summary>
@@ -36,7 +36,7 @@
         /// </summary>
         /// <param name="definitionId">The zone's definition ID</param>
         /// <param name="instanceId">The zone's instance ID, or 0 for non-instanced zones</param>
-        public DynamicZoneId(int definitionId, int instanceId)
+        public DynamicZoneId(ushort definitionId, ushort instanceId)
         {
             DefinitionId = definitionId;
             InstanceId = instanceId;
@@ -44,17 +44,17 @@
             ZoneId = ApplyBitShiftLeft(instanceId) | definitionId;
         }
 
-        private static int ApplyMask(int input, int bitMask)
+        private static uint ApplyMask(uint input, uint bitMask)
         {
-            return input & bitMask;
+            return (input & bitMask);
         }
 
-        private static int ApplyBitShiftRight(int input)
+        private static uint ApplyBitShiftRight(uint input)
         {
             return input >> 16;
         }
 
-        private static int ApplyBitShiftLeft(int input)
+        private static uint ApplyBitShiftLeft(uint input)
         {
             return input << 16;
         }
---
+++ test/PlanetsideSeaState.Shared.Tests/Planetside/DynamicZoneIdTests.cs
@@ -0,0 +1,78 @@
+﻿using PlanetsideSeaState.Shared.Planetside;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlanetsideSeaState.Shared.Tests.Planetside
+{
+    public class DynamicZoneIdTests
+    {
+        #region ZoneId Constructor
+        [Theory]
+        [InlineData(393577)]
+        [InlineData(459113)]
+        public void ZoneIdConstructor_DefinitionId_IsDesolation(uint zoneId)
+        {
+            var test = new DynamicZoneId(zoneId);
+
+            Assert.Equal(361, test.DefinitionId); // 0x0169
+        }
+
+        [Fact]
+        public void ZoneIdConstructor_DefinitionId_IsHossin()
+        {
+            var test = new DynamicZoneId(4);
+
+            Assert.Equal(4, test.DefinitionId);
+        }
+
+        [Theory]
+        [InlineData(393577, 6)]
+        [InlineData(459113, 7)]
+        public void ZoneIdConstructor_InstanceId_IsInstanceId(uint zoneId, ushort targetInstanceId)
+        {
+            var test = new DynamicZoneId(zoneId);
+
+            Assert.Equal(targetInstanceId, test.InstanceId);
+        }
+
+        [Fact]
+        public void ZoneIdConstructor_InstanceId_IsZero()
+        {
+            var test = new DynamicZoneId(4);
+
+            Assert.Equal(0, test.InstanceId);
+        }
+        #endregion ZoneId Constructor
+
+
+        #region Instance & Definition IDs Constructor
+        [Fact]
+        public void InstAndDefConstructor_DesolationSix_HasZoneId()
+        {
+            var test = new DynamicZoneId(361, 6);
+
+            Assert.Equal(393577U, test.ZoneId);
+        }
+
+        [Fact]
+        public void InstAndDefConstructor_DesolationSeven_HasZoneId()
+        {
+            var test = new DynamicZoneId(361, 7);
+
+            Assert.Equal(459113U, test.ZoneId);
+        }
+
+        [Fact]
+        public void InstAndDefConstructor_Hossin_HasZoneId()
+        {
+            var test = new DynamicZoneId(4, 0);
+
+            Assert.Equal(4U, test.ZoneId);
+        }
+        #endregion Instance & Definition IDs Constructor
+    }
+}
