870563f9cd80ba04e88e255945b18d03d3431156
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2021-10-07 10:46:33 -0500

    add GetAbbreviation method

--- src/PlanetsideSeaState.Shared/Constants/Faction.cs
+++ src/PlanetsideSeaState.Shared/Constants/Faction.cs
@@ -7,5 +7,17 @@
         public const short NC = 2;
         public const short TR = 3;
         public const short NSO = 4;
+
+        public static string GetAbbreviation(short? factionId)
+        {
+            return factionId switch
+            {
+                VS => "VS",
+                NC => "NC",
+                TR => "TR",
+                NSO => "NSO",
+                _ => "?"
+            };
+        }
     }
 }
