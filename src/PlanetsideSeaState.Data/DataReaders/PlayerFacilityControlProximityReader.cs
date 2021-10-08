c48c3465dd5f55f2826dc723379a6e9a0bba482d
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2021-10-08 16:55:33 -0500

    fix PlayerFacilityControlProximityReader referencing wrong column names for CharacterId and FacilityId

--- src/PlanetsideSeaState.Data/DataReaders/PlayerFacilityControlProximityReader.cs
+++ src/PlanetsideSeaState.Data/DataReaders/PlayerFacilityControlProximityReader.cs
@@ -11,8 +11,8 @@ namespace PlanetsideSeaState.Data.DataReaders
         {
             PlayerFacilityControlProximity control = new();
 
-            control.CharacterId = reader.GetString("acting_character_id");
-            control.FacilityId = reader.GetInt32("recipient_character_id");
+            control.CharacterId = reader.GetString("character_id");
+            control.FacilityId = reader.GetInt32("facility_id");
             control.Timestamp = reader.GetDateTime("timestamp");
             control.FacilityControlId = reader.GetGuid("facility_control_id");
             control.NewFactionId = reader.GetInt16("new_faction_id");
